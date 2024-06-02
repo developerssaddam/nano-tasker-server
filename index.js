0f97f80ca851866168edd4fbc7fd1096037f3713
developersaddam <developerssaddam@gmail.com>
2024-06-02 18:02:55 +0600

    Mongodb connection successfull

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.flkt4kr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Init Express
 const app = express();
 
 // Environment variables
-const port = process.env.PORT || 9090
+const port = process.env.PORT || 9090;
 
 // Middlewares
 app.use(express.json());
@@ -17,7 +19,31 @@ app.use(
   })
 );
 
+// Create a MongoClient for connection mongoDB
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(`MongoDB Connection is successfull!`.bgGreen.black);
+
+    // Test api
+    app.get("/", (req, res) => {
+      res.send(`NanoTasker server is running on port ${port}`);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Listen server
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`.bgMagenta.black);
-})
+  console.log(`Server is running on port: ${port}`.bgMagenta.black);
+});
