d048e8502819b3217bbbcce669103ec5e10839d3
developersaddam <developerssaddam@gmail.com>
2024-06-04 12:35:30 +0600

    Added get single user create new task and update user available coin route api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.flkt4kr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Init Express
@@ -15,9 +16,20 @@ const port = process.env.PORT || 9090;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://nano-tasker.web.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://nano-tasker.web.app",
+      "https://imgbb.com",
+    ],
   })
 );
+
+// tokenVerify
+const tokenVerify = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  console.log(token);
+  next();
+};
 
 // Create a MongoClient for connection mongoDB
 const client = new MongoClient(mongoURI, {
@@ -33,8 +45,56 @@ async function run() {
     await client.connect();
     console.log(`MongoDB Connection is successfull!`.bgGreen.black);
 
+    /*
+     *      Auth api
+     *  ====== x ==========
+     */
+
+    // Create jsonWebToken
+    app.post("/jwt/create", async (req, res) => {
+      const userEmail = req.query.email;
+      // create token
+      const token = jwt.sign(
+        { email: userEmail },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.send({ token });
+    });
+
     // User-Collection
     const userCollection = client.db("NanoTasker").collection("userCollection");
+    const taskCollection = client.db("NanoTasker").collection("taskCollection");
+
+    // Get all users
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get single user
+    app.get("/users/singleuser", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Update task creator user total coin when create a new task
+    app.put("/users/taskcreator", async (req, res) => {
+      const updatedData = req.body;
+      const { email, updatedCoin } = updatedData;
+      const query = { email: email };
+      const updateDoc = {
+        $set: {
+          totalCoin: updatedCoin,
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
     // Create user
     app.post("/users", async (req, res) => {
@@ -50,6 +110,17 @@ async function run() {
         const result = await userCollection.insertOne(userInfo);
         res.send(result);
       }
+    });
+
+    /*
+     *      TaskCreator api
+     *  ====== x ==========
+     */
+
+    app.post("/task/create", async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
     });
 
     // Test api
