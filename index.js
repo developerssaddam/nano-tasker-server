c1542a08c01f61a48414bc0c968c93b8e5982a5c
developersaddam <developerssaddam@gmail.com>
2024-06-08 09:08:13 +0600

    Create update sbumission action btn approve and reject api

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
       res.send(result);
     });
 
-    // Get single user
+    // Get single user by email
     app.get("/users/singleuser", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -135,7 +135,22 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/submission/create", async (req, res) => {
+    // Get submissionData from submissionCollection base on worker Email
+    app.get("/my/submission", tokenVerify, async (req, res) => {
+      const email = req.query.email;
+      const tokenEmail = req.email.email;
+      const query = { worker_email: email };
+
+      if (email !== tokenEmail) {
+        return res.status(401).send({ message: "Unauthorize-access!" });
+      } else {
+        const result = await submissionCollection.find(query).toArray();
+        res.send(result);
+      }
+    });
+
+    // Save data when an worker submission an single task
+    app.post("/submission/create", tokenVerify, async (req, res) => {
       const submissionDetails = req.body;
       const result = await submissionCollection.insertOne(submissionDetails);
       res.send(result);
@@ -214,6 +229,83 @@ async function run() {
       const { id } = req.params;
       const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Get all pending submission data where is match taskCreator email
+    app.get("/mytask/worker/submission", tokenVerify, async (req, res) => {
+      const tokenEmail = req.email.email;
+      const email = req.query.email;
+
+      // validate token mail
+      if (tokenEmail !== email) {
+        return res.status(401).send({ message: "Unauthorize-access!" });
+      }
+
+      const query = { creator_email: email };
+
+      // Get all submission data from submissionCollection by creator_email match.
+      const myTask = await submissionCollection.find(query).toArray();
+
+      // Now filter data which is status is pending;
+      const result = myTask.filter((task) => task.status === "Pending");
+      res.send(result);
+    });
+
+    // Get single SubmissionData by id (id get query)
+    app.get("/single/submission/data", async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await submissionCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Update worker totalCoin and submission status approved;
+    app.patch(
+      "/update/worker/totalcoin/and/submission/approve",
+      async (req, res) => {
+        const data = req.body;
+        const { _id, amount, worker_email } = data;
+        const queryOne = { _id: new ObjectId(_id) };
+        const queryTwo = { email: worker_email };
+        const worker = await userCollection.findOne(queryTwo);
+
+        const updatedStatus = {
+          $set: {
+            status: "Approve",
+          },
+        };
+
+        const increaseWorkerCoin = {
+          $set: {
+            totalCoin: worker?.totalCoin + amount,
+          },
+        };
+
+        const updateStatus = await submissionCollection.updateOne(
+          queryOne,
+          updatedStatus
+        );
+
+        const increaseWorkerTotalCoin = await userCollection.updateOne(
+          queryTwo,
+          increaseWorkerCoin
+        );
+
+        res.send({ updateStatus, increaseWorkerTotalCoin });
+      }
+    );
+
+    // Update worker submission status to rejected
+    app.patch("/update/worker/status/rejected", async (req, res) => {
+      const { id } = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: "Rejected",
+        },
+      };
+      const result = await submissionCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
 
